a8de9c176594a447a4237afaae014f7f4ca4877e
evgenym <evgenymyz@gmail.com>
2021-07-30 16:16:25 +0300

    new updates for heroku deployment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cxxrydf%@-4m=c*a(f#mv8*4n@r5@f5++v!^z_)$1n9s!di4*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
